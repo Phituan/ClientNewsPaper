c3d8239474e938eec0a4361c7aa71cd06bb325f1
phituan <tuandpd00617@fpt.edu.vn>
2019-07-30 20:24:51 +0700

    sualoi-lay-tin

--- js/latestPost.js
+++ js/latestPost.js
@@ -2,7 +2,7 @@ getNews();
 
 function getNews() {
     var game_content = document.getElementById("latestpost");
-    var getNewsPaper = "https://beta-dot-assignmentcrawler.appspot.com/_api/article";
+    var getNewsPaper = "https://beta-dot-assignmentcrawler.appspot.com/_api/article?ct=more";
     if (localStorage.getItem) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
@@ -10,7 +10,7 @@ function getNews() {
 
             if (this.readyState == 4 && this.status == 200) {
                 var obj = JSON.parse(xhttp.responseText);
-                for (var i = 0; i < obj.data.length; i++) {
+                for (var i = 0; i < 8; i++) {
                     var image;
                     if (obj.data[i].thumbnail == 0) {
                         image = "https://q5v8e3s3.stackpathcdn.com/wp-content/uploads/2019/04/news-default-image-300x225.jpg";
@@ -22,7 +22,7 @@ function getNews() {
                     news_item += '</div>';
                     news_item += '<div class="post-data">';
                     news_item += '<div class="post-meta">';
-                    news_item += '<a class="post-title"><h6>' + obj.data[i].title + '</h6></a>';
+                    news_item += '<a href="" class="post-title"><h6>' + obj.data[i].title + '</h6></a>';
                     news_item += '<p class="post-date"><span>' + obj.data[i].createdAtMLS +' </span></p>';
                     news_item += '</div>';
                     news_item += '</div>';
